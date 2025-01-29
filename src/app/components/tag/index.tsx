e727a197cec4369bb465342eb76648766ebcf5ab
Holices Junior <dev.holices@gmail.com>
2025-01-29 11:20:02 +0000

    build: created tag component

--- src/app/components/tag/index.tsx
+++ src/app/components/tag/index.tsx
@@ -4,6 +4,6 @@ interface TagProps {
 
 export const Tag = ({ title }: TagProps) => {
   return (
-    <span className="py-[6px] px-3 bg-zinc-800 rounded-full text-sm font-medium">{title}</span>
+    <span className="py-[6px] px-3 bg-zinc-700 rounded-full text-sm font-medium">{title}</span>
   )
 }

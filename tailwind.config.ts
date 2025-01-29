79a6b2aa85ee8166a1a66451ea99ab8c9b318798
Holices Junior <dev.holices@gmail.com>
2025-01-29 11:21:46 +0000

    chore: add tailwind safelist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    { pattern: /^w-\d+$/ }, // Permite classes como w-12, w-16, w-20...
+    { pattern: /^h-\d+$/ }  // Permite classes como h-12, h-16, h-20...
+  ],
   theme: {
     extend: {},
   },

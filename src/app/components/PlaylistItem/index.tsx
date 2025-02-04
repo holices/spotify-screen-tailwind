af289053a56dd187f2a63bcd38ea7c3de7160f85
Holices Junior <dev.holices@gmail.com>
2025-02-04 18:05:01 +0000

    feat: add PlaylistItem component

--- src/app/components/PlaylistItem/index.tsx
+++ src/app/components/PlaylistItem/index.tsx
@@ -1,13 +1,13 @@
 import Image, { StaticImageData } from "next/image";
 
-interface CardMusicProps {
+interface MusicCardProps {
   logo: string | StaticImageData;
   title: string;
   description: string;
   size?: number;
 }
 
-export const PlaylistCard = ({ logo, title, description, size = 12 }: CardMusicProps) => {
+export const PlaylistItem = ({ logo, title, description, size = 12 }: MusicCardProps) => {
   return (
     <div className="flex gap-3 ">
       <Image 
@@ -15,9 +15,9 @@ export const PlaylistCard = ({ logo, title, description, size = 12 }: CardMusicP
         alt='something' 
         className={`rounded-md w-${size} h-${size}`} 
       />
-      <div className="flex flex-col justify-center max-w-48">
+      <div className="flex flex-col justify-center w-48">
         <h2 className="text-zinc-50 font-medium">{title}</h2>
-        <p className="text-zinc-300 truncate">{description}</p>
+        <p className="text-zinc-300 text-sm truncate">{description}</p>
       </div>
     </div>
   )

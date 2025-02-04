3b0947beb396fc81241f5de0e68bd4c96309254e
Holices Junior <dev.holices@gmail.com>
2025-02-04 18:12:58 +0000

    removing index.tsx

--- src/app/components/tag/index.tsx
+++
@@ -1,9 +0,0 @@
-interface TagProps {
-  title: string;
-}
-
-export const Tag = ({ title }: TagProps) => {
-  return (
-    <span className="py-[6px] px-3 bg-zinc-700 rounded-full text-sm font-medium">{title}</span>
-  )
-}
